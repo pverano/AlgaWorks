2372e082b3080a5978beec51c06583bade8e67f4
paulo verano <paulo.verano@hotmail.com>
2021-01-20 15:19:15 -0300

    primeiro commit

---
+++ src/test/java/com/example/algaworks/AlgaworksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.algaworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgaworksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
