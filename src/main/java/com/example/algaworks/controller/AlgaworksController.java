016e5a4763a1c6a63cc268472e41de754ecd5c30
paulo verano <paulo.verano@hotmail.com>
2022-03-03 18:56:36 -0300

    adicionando pesquisa personalizada e debbug de sql

--- src/main/java/com/example/algaworks/controller/AlgaworksController.java
+++ src/main/java/com/example/algaworks/controller/AlgaworksController.java
@@ -3,12 +3,14 @@ package com.example.algaworks.controller;
 import com.example.algaworks.modelo.TB_CLIENTE;
 import com.example.algaworks.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("cliente")
+@RequestMapping("clientes")
 public class AlgaworksController {
 
     @Autowired
@@ -18,9 +20,25 @@ public class AlgaworksController {
     public List<TB_CLIENTE> listAll() {
         return clienteRepository.findAll();
     }
+    @GetMapping("/{id}")
+    public Optional<TB_CLIENTE> findById(@PathVariable("id") Long id) {
+        return clienteRepository.findById(id);
+    }
+    @GetMapping("/nome/{nome}")
+    public Optional<List<TB_CLIENTE>> findByNome(@PathVariable("nome") String nome) {
+        return clienteRepository.findByNome(nome);
+    }
+    @GetMapping("/idade/{idade}")
+    public List<TB_CLIENTE> findByIdade(@PathVariable("idade")Integer idade) {
+        return clienteRepository.findByidade(idade);
+    }
 
     @PostMapping
     public TB_CLIENTE save(@RequestBody TB_CLIENTE cliente) {
         return clienteRepository.save(cliente);
+    }
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id")Long id) {
+        clienteRepository.deleteById(id);
     }
 }
--- src/main/java/com/example/algaworks/repository/ClienteRepository.java
+++ src/main/java/com/example/algaworks/repository/ClienteRepository.java
@@ -2,8 +2,17 @@ package com.example.algaworks.repository;
 
 import com.example.algaworks.modelo.TB_CLIENTE;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface ClienteRepository extends JpaRepository<TB_CLIENTE,Long> {
+public  interface ClienteRepository extends JpaRepository<TB_CLIENTE,Long> {
+    @Query("from TB_CLIENTE cli where cli.nome=:nome")
+    Optional<List<TB_CLIENTE>> findByNome(@Param("nome") String nome);
+    @Query("select cli from TB_CLIENTE cli where cli.idade=:idade")
+    List<TB_CLIENTE> findByidade(@Param("idade") Integer idade);
 }
