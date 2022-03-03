4f3a2ad9d6c1dfd8205d1447dbc2690701553f85
paulo verano <paulo.verano@hotmail.com>
2022-03-03 20:17:05 -0300

    adicionado metodo put com tratamento se existe o cliente

--- src/main/java/com/example/algaworks/controller/AlgaworksController.java
+++ src/main/java/com/example/algaworks/controller/AlgaworksController.java
@@ -3,7 +3,7 @@ package com.example.algaworks.controller;
 import com.example.algaworks.modelo.TB_CLIENTE;
 import com.example.algaworks.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,25 +20,39 @@ public class AlgaworksController {
     public List<TB_CLIENTE> listAll() {
         return clienteRepository.findAll();
     }
+
     @GetMapping("/{id}")
     public Optional<TB_CLIENTE> findById(@PathVariable("id") Long id) {
         return clienteRepository.findById(id);
     }
+
     @GetMapping("/nome/{nome}")
     public Optional<List<TB_CLIENTE>> findByNome(@PathVariable("nome") String nome) {
         return clienteRepository.findByNome(nome);
     }
+
     @GetMapping("/idade/{idade}")
-    public List<TB_CLIENTE> findByIdade(@PathVariable("idade")Integer idade) {
+    public List<TB_CLIENTE> findByIdade(@PathVariable("idade") Integer idade) {
         return clienteRepository.findByidade(idade);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TB_CLIENTE> atualizaCliente(@PathVariable("id") Long id, @RequestBody TB_CLIENTE cliente) {
+        if (clienteRepository.findById(id).isPresent()) {
+            cliente.setId(id);
+            return ResponseEntity.ok().body(clienteRepository.save(cliente));
+        } else {
+            return ResponseEntity.noContent().build();
+        }
     }
 
     @PostMapping
     public TB_CLIENTE save(@RequestBody TB_CLIENTE cliente) {
         return clienteRepository.save(cliente);
     }
+
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable("id")Long id) {
+    public void delete(@PathVariable("id") Long id) {
         clienteRepository.deleteById(id);
     }
 }
--- src/main/java/com/example/algaworks/modelo/TB_CLIENTE.java
+++ src/main/java/com/example/algaworks/modelo/TB_CLIENTE.java
@@ -1,14 +1,12 @@
 package com.example.algaworks.modelo;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Getter
 @Setter
+@Builder
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
